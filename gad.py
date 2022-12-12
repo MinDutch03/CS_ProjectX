af19c27724e97cb6e42254a5046a252ec5115491
Duc Nguyen <nguyenminhduc890@gmail.com>
2022-12-12 18:16:13 +0700

    Update gad.py

--- gad.py
+++ gad.py
@@ -4,7 +4,7 @@ import time
 import matplotlib.pyplot as plt
 import os
 from logging import exception
-import  sys
+import sys
 #from  Adafruit_IO import  MQTTClient
 #from sqlalchemy import except_
 
@@ -48,7 +48,8 @@ ageModel = "modelNweight/age_net.caffemodel"
 genderProto = "modelNweight/gender_deploy.prototxt"
 genderModel = "modelNweight/gender_net.caffemodel"
 
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746) # model value
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744,
+                     114.895847746)  # model value
 
 ageList = [
     '(0-6)', '(6-13)', '(14-19)', '(20-29)',
@@ -69,18 +70,17 @@ faceNet = cv2.dnn.readNet(faceModel, faceProto)
 padding = 20
 
 
-
 # defining function
 def getFaceBox(net, frame, conf_threshold=0.7):
-    ''' 
-    Detect the face of the person inside the image and Output a bounding box 
+    '''
+    Detect the face of the person inside the image and Output a bounding box
     encapsulating the face detected.
     '''
 
     frameOpencvDnn = frame.copy()
     frameHeight = frameOpencvDnn.shape[0]
     frameWidth = frameOpencvDnn.shape[1]
-    
+
     blob = cv2.dnn.blobFromImage(
         frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False
     )
@@ -105,7 +105,7 @@ def getFaceBox(net, frame, conf_threshold=0.7):
                 int(round(frameHeight / 150)),
                 8,
             )
-    
+
     return frameOpencvDnn, bboxes
 
 
@@ -113,15 +113,15 @@ def age_gender_detector(frame):
     '''
     Get face of the person inside inputted `frame` and predict their `gender` and `age`.
 
-    - Age is categorised into several groups, which are determined based on the 
-    distiction of the face in some age periods throughout their life. 
+    - Age is categorised into several groups, which are determined based on the
+    distiction of the face in some age periods throughout their life.
     All group age: `(0-6)`, `(6-13)`, `(14-19)`, `(20-29)`, `(30- 39)`, `(40-55)`, `(56-60)`, `(61-70)`, `(71-100)`.
-    
+
     - Gender is categorised into only `Male` and `Female`.
 
     ------------------------------------------------------------------------------------------
     Output:
-    - Print out the predicted age, gender and the confidence of the prediction for each instance. 
+    - Print out the predicted age, gender and the confidence of the prediction for each instance.
     '''
 
     # Read frame
@@ -145,37 +145,36 @@ def age_gender_detector(frame):
         genderNet.setInput(blob)
         genderPreds = genderNet.forward()
         gender = genderList[genderPreds[0].argmax()]
-        
+
         print("Gender : {}, conf = {:.3f}".format(
             gender, genderPreds[0].max()))
 
         ageNet.setInput(blob)
         agePreds = ageNet.forward()
-        
+
         age = ageList[agePreds[0].argmax()]
 
         #print("Age Output : {}".format(agePreds))
         print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
 
         # store values for iot
-        
-        print("Update Age:", age) # check age name
+
+        print("Update Age:", age)  # check age name
         # client.publish("Age", age)
 
-        print("Update Gender:", gender) # check age name
+        print("Update Gender:", gender)  # check age name
         # client.publish("Gender", gender)
-        
+
         conf_age = agePreds[0].max()
-        print("Update Age Confidence:", conf_age) # check age name
+        print("Update Age Confidence:", conf_age)  # check age name
         # client.publish("Age_Confidence", conf_age)
-        
+
         conf_gender = genderPreds[0].max()
-        print("Update Confidence:", conf_gender) # check age name
+        print("Update Confidence:", conf_gender)  # check age name
         # client.publish("Gender_confidence", conf_gender)
-        
 
         label = "{} , {}".format(gender, age)
-        cv2.putText(
+        '''cv2.putText(
             img= frameFace,
             text= label,
             org= (bbox[0], bbox[1] - 10),
@@ -185,7 +184,7 @@ def age_gender_detector(frame):
             thickness= 2,
             # cv2. LINE_AA gives anti-aliased line.
             lineType= cv2.LINE_AA,
-        )
+        )'''
     return frameFace
 
 
@@ -205,15 +204,15 @@ def show_results(folder):
         # results
         output = age_gender_detector(img)
         rgb_output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
-        # plt.figure(figsize=(8, 8))
-        # plt.imshow(rgb_output)
-        # plt.show()
+        plt.figure(figsize=(10, 10))
+        plt.imshow(rgb_output)
+        plt.show()
 
 
 # show_results("./celeba-dataset/img_align_celeba/img_align_celeba")
 show_results("./img")
 
-#print(predicted_age)
-#print(predicted_gender)
-#print(confidenceAge)
-#print(confidenceGender)
+# print(predicted_age)
+# print(predicted_gender)
+# print(confidenceAge)
+# print(confidenceGender)
