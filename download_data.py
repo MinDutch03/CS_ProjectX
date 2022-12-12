90630ffe9d6dad0b91c4013bf5e7637cd4805f3e
Duc Nguyen <nguyenminhduc890@gmail.com>
2022-12-13 00:47:54 +0700

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,11 +1,11 @@
-'''import opendatasets as od
+import opendatasets as od
 import zipfile
 
 # download data from kaggle
 od.download(
-    'https://www.kaggle.com/datasets/jessicali9530/celeba-dataset', force=True)
+    'https://www.kaggle.com/datasets/ttungl/adience-benchmark-gender-and-age-classification', force=True)
 
 # unzip file
-Dataset = "celeba-dataset"
-with zipfile.ZipFile("./celeba-dataset/"+Dataset+".zip", "r") as z:
-    z.extractall(".")'''
+Dataset = "Adience-benchmark-gender-and-age-classification"
+with zipfile.ZipFile("./adience-benchmark-gender-and-age-classification/"+Dataset+".zip", "r") as z:
+    z.extractall(".")
