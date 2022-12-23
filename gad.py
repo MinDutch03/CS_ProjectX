34be961cbb2801027a01c46dc6f7ea8815d4d2df
Duc Nguyen <nguyenminhduc890@gmail.com>
2022-12-23 13:27:57 +0700

    Update gad.py

--- gad.py
+++ gad.py
@@ -151,7 +151,7 @@ def age_gender_detector(frame):
         # setInput: Sets the new input value for blob (aka outputted preprocessed image).
         genderNet.setInput(blob)
         # genderPreds = gender Prediction
-        # forward: runs forward pass to compute output of layer with the genderNetwork
+        # forward(): runs forward pass to compute output of layer with the genderNetwork
         genderPreds = genderNet.forward()
         gender = genderList[genderPreds[0].argmax()]
 
